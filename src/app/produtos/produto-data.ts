dfb2141fcf396c1021f63cea44e808d2c7e461d9
Diego <diegoteixeirapereira@hotmail.com>
2020-12-10 08:48:47 -0300

    alteração nos produtos da base de dados

--- src/app/produtos/produto-data.ts
+++ src/app/produtos/produto-data.ts
@@ -6,78 +6,78 @@ export class ProdutoData implements InMemoryDbService {
     const produtos: Produto[] = [
       {
         id: 1,
-        produtoNome: 'Reloj plateado',
-        descricao: 'Reloj de pulsera, color plateado, con números romanos',
+        produtoNome: 'Relógio de prata',
+        descricao: 'Relógio de pulso, cor prata, com números romanos',
         preco: 80000,
         imagemUrl: 'assets/images/product1.jpg'
       },
       {
         id: 2,
-        produtoNome: 'Cinturón con águila',
-        descricao: 'Color gris, con hebilla metalica en forma de águila',
+        produtoNome: 'Cinto com águia',
+        descricao: 'Cor cinza, com fivela de metal em forma de águia',
         preco: 20000,
         imagemUrl: 'assets/images/product2.jpg'
       },
       {
         id: 3,
-        produtoNome: 'Reloj despertador',
-        descricao: 'Reloj de mesa, color gris, con números romanos',
+        produtoNome: 'Relógio despertador',
+        descricao: 'Relógio de mesa, cor cinza, com números romanos',
         preco: 50000,
         imagemUrl: 'assets/images/product3.jpg'
       },
       {
         id: 4,
         produtoNome: 'Suéter',
-        descricao: 'Suéter de lana, con líneas grises y azules',
+        descricao: 'Suéter de lã, com linhas cinza e azuis',
         preco: 60000,
         imagemUrl: 'assets/images/product4.jpg'
       },
       {
         id: 5,
-        produtoNome: 'Juego de corbatas',
-        descricao: 'Corbatas con diferentes figuras y colores',
+        produtoNome: 'Conjunto de gravatas',
+        descricao: 'Gravatas com diferentes formas e cores',
         preco: 70000,
         imagemUrl: 'assets/images/product5.jpg'
       },
       {
         id: 6,
-        produtoNome: 'Reloj inteligente',
-        descricao: 'Sumergible, ritmo cardíaco, bluetooth',
+        produtoNome: 'Relógio inteligente',
+        descricao: 'Submersível, ritmo cardíaco, bluetooth',
         preco: 230000,
         imagemUrl: 'assets/images/product6.jpg'
       },
       {
         id: 7,
-        produtoNome: 'Reloj en acero',
-        descricao: 'Reloj plateado con azul, sumergible 10 metros',
+        produtoNome: 'Relógio de aço',
+        descricao: 'Relogio prateado com azul, Submersível 10 metros',
         preco: 175000,
         imagemUrl: 'assets/images/product7.jpg'
       },
       {
         id: 8,
-        produtoNome: 'Tennis de tela',
-        descricao: 'Tennis negros con adornos color blanco',
+        produtoNome: 'Tênis de Mesh',
+        descricao: 'Tênis pretos com enfeites brancos',
         preco: 40000,
         imagemUrl: 'assets/images/product8.jpg'
       },
       {
         id: 9,
-        produtoNome: 'Zapatos para bebe',
-        descricao: 'Color verde con cafe, suela antideslizante',
+        produtoNome: 'Tênis infantil',
+        descricao: 'Cor verde com café, sola antiderrapante',
         preco: 67500,
         imagemUrl: 'assets/images/product9.jpg'
       },
       {
         id: 10,
-        produtoNome: 'Reloj café y corbata',
-        descricao: 'Reloj de pulsera en cuero color café, corbata color gris',
+        produtoNome: 'Conjunto relógio cor café e gravata',
+        descricao: 'Relógio de pulso de couro marrom, gravata cinza',
         preco: 310000,
         imagemUrl: 'assets/images/product10.jpg'
       },
       {
         id: 11,
-        produtoNome: 'Tennis para mujer',
-        descricao: 'Tennis rosados con líneas blancas, cordones rosados',
+        produtoNome: 'Tênis feminino',
+        descricao: 'Tênis rosa com linhas brancas, cadarços rosa',
         preco: 77000,
         imagemUrl: 'assets/images/product11.jpg'
       }
